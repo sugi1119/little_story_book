aa24f774403dfa77410ab591ca3fda4b79d0cc80
Yuko Sugiyama <sugi_yuko@hotmail.com>
2014-09-01 00:45:41 +1000

    add book model / controller / view

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,63 @@
+class BooksController < ApplicationController
+  before_action :authenticate_author!
+
+  def index
+    @books = Book.all
+
+    respond_to do |format|
+      format.html { }
+      format.json { render :json => @books }
+    end
+  end
+
+  def new
+    @book = Book.new
+    @book.author_id = current_author.id
+
+
+  end
+
+  def create
+    @book = Book.new book_params
+    @book.author_id = current_author.id
+
+    if @book.save
+      redirect_to @book
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @book = Book.find params[:id]
+
+
+  end
+
+  def show
+    @book = Book.find params[:id]
+
+
+    # raise 'error'
+  end
+
+  def update
+    book = Book.find params[:id]
+    book.update book_params
+
+    redirect_to book
+  end
+
+  def destroy
+    book = Book.find params[:id]
+    book.destroy
+
+    flash[:notice] = "The book is now deleted. "
+    redirect_to books_path
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :author_id)
+    end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140831133249) do
+ActiveRecord::Schema.define(version: 20140831144118) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,5 +34,12 @@ ActiveRecord::Schema.define(version: 20140831133249) do
 
   add_index "authors", ["email"], name: "index_authors_on_email", unique: true, using: :btree
   add_index "authors", ["reset_password_token"], name: "index_authors_on_reset_password_token", unique: true, using: :btree
+
+  create_table "books", force: true do |t|
+    t.string   "title"
+    t.integer  "author_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
